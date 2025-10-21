9b0405acd94cae0c10f1d945ff7ad609cc59d3d1
Dale Peligro <daledoesminecraft2013@gmail.com>
2025-10-20 23:31:55 -0700

    Added List of prereq's

---
+++ src/data/Course.java
@@ -0,0 +1,76 @@
+package src.data;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// Class for each course
+public class Course {
+    private String name;
+    private String courseMajor;
+    private int courseNum;
+    private int courseId;
+    private int units;
+    private CourseCategory category;
+    private List<Course> prerequisites;
+    Random random = new Random();
+
+    public Course(String courseMajor, int courseNum, String name, int units) {
+        this.courseMajor = courseMajor;
+        this.courseNum = courseNum;
+        this.name = name;
+        this.units = units;
+        this.courseId = random.nextInt(1000000) + random.nextInt(1000000);
+        this.prerequisites = new ArrayList<>();
+    }
+
+    public Course(String courseMajor, int courseNum, String name, int units, List<Course> prerequisites) {
+        this.courseMajor = courseMajor;
+        this.courseNum = courseNum;
+        this.name = name;
+        this.units = units;
+        this.courseId = random.nextInt(1000000) + random.nextInt(1000000);
+        this.prerequisites = (prerequisites != null) ? prerequisites : new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public CourseCategory getCourseCategory() {
+        return category;
+    }
+
+    public List<Course> getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
+
+    public void addPrerequisite(Course course) {
+        prerequisites.add(course);
+    }
+
+    public boolean hasPrerequisites() {
+        return !prerequisites.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return courseMajor + courseNum + " " + name;
+    }
+
+}

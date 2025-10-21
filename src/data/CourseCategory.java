b88333e0eb6a6448aa9d1c1080918738b4df8a68
Dale Peligro <daledoesminecraft2013@gmail.com>
2025-10-20 23:32:10 -0700

    added enum for course categories

---
+++ src/data/CourseCategory.java
@@ -0,0 +1,11 @@
+package src.data;
+
+public enum CourseCategory {
+    GENERAL_EDUCATION,
+    MAJOR,
+    MAJOR_ELECTIVE;
+
+    public CourseCategory getCategory() {
+        return this;
+    }
+}

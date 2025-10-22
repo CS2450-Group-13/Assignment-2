9a9f6aea4060cce2e460f7a9791ae991b638e57e
Dale Peligro <daledoesminecraft2013@gmail.com>
2025-10-21 23:46:42 -0700

    added basic nav layout

--- README.md
+++ README.md
@@ -2,13 +2,7 @@
 Critique, Redesign, Prototype for CPP's Degree Progress Report 
 
 ## To run the program (powershell)
-<<<<<<< HEAD
 .\run.ps1
-=======
-javac -d out (Get-ChildItem -Recurse -Filter *.java src).FullName
-
-java -cp out src.Main
->>>>>>> 9345e8bf50136a086399b20945c7d1e27321b9de
 
 
 (Run from the root file, do NOT be in src/..)
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ package src;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import src.data.Course;
 import src.data.CourseCategory;
@@ -41,19 +44,51 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        // Info Page
-        BorderPane infoPage = new BorderPane();
-        VBox header = new VBox();
-        HBox navBar = new HBox();
+        // ----------- | NAV BAR | -----------
+        // title of app
         Label title = new Label("Degree Progress");
         title.setId("app-title");
 
+        // nav elements
+        VBox header = new VBox();
+        HBox navBar = new HBox();
         header.getChildren().addAll(title, navBar);
         header.setAlignment(Pos.CENTER);
 
-        infoPage.setTop(header);
+        Label infoLabel = new Label("Student Info");
+        Label genEdLabel = new Label("General Education");
+        Label majorCoursesLabel = new Label("Major Courses");
+        Label majorElecLabel = new Label("Major Electives");
+        Label catalogueLabel = new Label("Course Catalogue");
 
-        Scene scene = new Scene(infoPage, 600, 750);
+        navBar.getChildren().addAll(
+            infoLabel, 
+            genEdLabel, 
+            majorCoursesLabel,
+            majorElecLabel,
+            catalogueLabel
+            );
+
+        navBar.setSpacing(90);
+        navBar.setAlignment(Pos.CENTER);
+        navBar.setPadding(new Insets(10, 0, 0, 0));
+
+        for (Node child: navBar.getChildren()) {
+            child.getStyleClass().add("nav-bar-item");
+        }
+
+        // ----------- | PAGES | -----------
+        StackPane pages = new StackPane();
+
+        //
+        
+        // ----------- | ROOT | -----------
+        // instantiate root node and layout
+        BorderPane root = new BorderPane();
+        root.setTop(header);
+        root.setCenter(pages);
+
+        Scene scene = new Scene(root, 1250, 1000);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
